dff7a74b5173f49629e799db9c35e0585c788273
diegosalazar00 <57643277+diegosalazar00@users.noreply.github.com>
2020-02-03 15:39:01 -0600

    clase arbol

---
+++ Lab2E2/Lab2E2/Models/Arbol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2E2;
+namespace Lab2E2.Models
+{
+    public class Arbol<BebidaClase> where BebidaClase : Icomparable
+    {
+        public int grado;
+
+        public Arbol()
+        {
+            grado = 5;
+        }
+        
+        public int root { get; set; }
+
+        public search(BebidaClase objeto)
+        {
+            BebidaClase dato;
+            dato = Nodo.search(objeto);
+            return dato;
+        }
+    }
+}
