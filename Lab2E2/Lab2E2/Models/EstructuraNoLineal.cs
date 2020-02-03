7d015fd771d950442102007e714efdd2dc04129b
Andres Guzman <j.andres.guzman0021@gmail.com>
2020-02-03 14:36:26 -0600

    Estructura no lineal

--- Lab2E2/Lab2E2/Models/EstructuraNoLineal.cs
+++ Lab2E2/Lab2E2/Models/EstructuraNoLineal.cs
@@ -5,7 +5,11 @@ using System.Threading.Tasks;
 
 namespace Lab2E2.Models
 {
-    public class EstructuraNoLineal
+    public class EstructuraNoLineal<Nodo> where Nodo : IComparable
     {
+        public int Grado { get; protected set; }
+       
+
     }
+    
 }
---
+++ Lab2E2/Lab2E2/Models/Nodo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2E2.Models
+{
+    public class Nodo<T> where  T:IComparable
+    {
+        int posicion { get; set;}
+        int anterior { get; set;}
+        int siguiente { get; set; }
+        T dato { get; set; }
+
+    }
+}
