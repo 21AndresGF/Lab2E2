ede6a29a019831e06aaee224a6f046f97c19349e
Andres Guzman <j.andres.guzman0021@gmail.com>
2020-02-05 22:40:50 -0600

    estructura de la clase nodo metodo get root

--- Lab2E2/Lab2E2/Models/Nodo.cs
+++ Lab2E2/Lab2E2/Models/Nodo.cs
@@ -5,16 +5,43 @@ using System.Threading.Tasks;
 
 namespace Lab2E2.Models
 {
-    public class Nodo<T> where  T:IComparable
+    public class Nodo<T> where T : IComparable
     {
-        public List<T> Datos = new List<T>();
-        List<Nodo<T>> Hijos = new List<Nodo<T>>();
+        int posicion;
+        int rango;
+        public List<List<T>> valores;
+        public List<T> Datos;
+        public List<int> hijos;        
+        public List<int> id;
+        public List<int> padre;
 
-        public void insertar(T dato) 
+        bool isLeaf;
+        public Nodo(int dato) 
+        {
+            rango = dato;
+            Datos = new List<T>();
+            hijos = new List<int>();
+            id = new List<int>();
+            posicion = 1;
+
+        }
+        public int Getroot() 
+        {
+            int iconteo = 0;
+            for (int i = 0; i < padre.Count; i++)
+            {
+                if (padre[i] == 0)
+                {
+                    iconteo = i;
+                }
+            }
+            return iconteo;
+        }
+        public void insertar(T dato, int root) 
         {
             
         }
-        public T search(T dato) 
+        public T search(T dato, Nodo<T> arreglo) 
         {
             return dato;
         }
