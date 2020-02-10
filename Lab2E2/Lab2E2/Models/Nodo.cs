8c2239d2cd5af9d8de46e7bf22cb933157cc6d7a
Andres Guzman <j.andres.guzman0021@gmail.com>
2020-02-09 19:38:31 -0600

    desarrollo de nodo

--- Lab2E2/Lab2E2/Models/Nodo.cs
+++ Lab2E2/Lab2E2/Models/Nodo.cs
@@ -7,23 +7,28 @@ namespace Lab2E2.Models
 {
     public class Nodo<T> where T : IComparable
     {
+        #region variables globales
         int posicion;
         int rango;
-        public List<List<T>> valores;
         public List<T> Datos;
+        public List<List<T>> valores;
         public List<List<int>> hijos;
         public List<int> id;
         public List<int> padre;
-
+        #endregion
+        #region constructor
         public Nodo(int dato)
         {
             rango = dato;
+            valores = new List<List<T>>();
             Datos = new List<T>();
             hijos = new List<List<int>>();
             id = new List<int>();
             posicion = 1;
 
         }
+        #endregion
+        #region ubicacion de datos
         public int Getroot()
         {
             int iconteo = 0;
@@ -36,6 +41,8 @@ namespace Lab2E2.Models
             }
             return iconteo;
         }
+        #endregion
+        #region padre y hermano
         public void NewBrother(int posicion, int hermano)
         {
             List<int> aux = new List<int>();
@@ -49,28 +56,27 @@ namespace Lab2E2.Models
             {
                 padre.Add(padre[posicion + 2]);
                 hijos[padre[hermano]].Add(posicion + 1);
+                hijos[hermano] = new List<int>();
                 aux.AddRange(hijos[padre[hermano]]);
                 hijos[padre[hermano]].RemoveRange(hijos.IndexOf(hijos[padre[hermano]]),hijos[padre[hermano]].Count);
 
             }
-            
-
-
         }
         public void NewFather()
         {
             id.Add(posicion + 2);
             posicion = posicion + 2;
-
-
         }
-        
+        #endregion 
         public void insertar(T dato, int nodo)
         {
+            //creacion variable local temporal de datos de tipo T
             List<T> temp = new List<T>();
-            //manejo por casos
+            //manejo de informacion, se incerta el dato cambiado la metodologia segun sus 
+            //banderas definidas, tiene padre? es hoja?
             valores[nodo].Add(dato);
             valores[nodo].Sort();
+
             if (IsFull(nodo))
             {
                 temp.AddRange(valores[nodo]);
@@ -91,7 +97,16 @@ namespace Lab2E2.Models
             }
             
         }
+        public int GetNodo(T dato, int root) 
+        {
+            int hoja = 0;
+            foreach (T datos in valores[root])
+            {
 
+            }
+
+            return hoja;
+        }
         public T search(T dato, Nodo<T> arreglo) 
         {
             return dato;
